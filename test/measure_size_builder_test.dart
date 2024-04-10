70e3051587bc9badf4372b629499cc85152453e4
XimYa <hj_lee_com@naver.com>
2024-04-10 21:59:36 +0900

    chore : add example code

--- test/measure_size_builder_test.dart
+++ test/measure_size_builder_test.dart
@@ -1,12 +1,30 @@
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:measure_size_builder/measure_size_builder.dart';
-//
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//   });
-// }
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:measure_size_builder/measure_size_builder.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: MeasureSizeBuilder(
+          builder: (context, size) {
+            log('Widget Size that changes dynamically : $size');
+            return const ExpansionTile(
+              title: Text('ExpansionTile'),
+              subtitle: Text('Trailing expansion arrow icon'),
+              children: <Widget>[
+                ListTile(
+                  title: Text('This is tile number 1'),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
